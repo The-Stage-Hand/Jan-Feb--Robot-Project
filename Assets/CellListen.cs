c2b713732b85bd7bb428f15b6a34a9bd24868a88
Purotheotherguy <ctblova8690@gmail.com>
2026-01-27 13:44:01 -0500

    Add CellListen and FunctionDo Unity scripts
    Introduced two new MonoBehaviour scripts: CellListen with placeholder action methods, and FunctionDo with a basic action selection structure. These scripts lay the groundwork for future functionality in the Unity project.

---
+++ Assets/CellListen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellListen : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Action1()
+	{
+
+	}
+	void Action2()
+	{
+
+	}
+	void Action3()
+	{
+
+	}
+}
---
+++ Assets/FunctionDo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionDo : MonoBehaviour {
+
+
+	int action = 0; // used to assign action
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Action(int action)
+	{
+		if (action == 0)
+		{
+		}
+		if (action == 1)
+		{
+		}
+	}
+}
